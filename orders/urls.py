23a9437de652ddb8766aa08e0a22db78781952ba
bb <btripleb@mail.ru>
2018-03-15 01:57:36 +0600

    srs django

---
+++ orders/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+from landing import views
+
+urlpatterns = [
+    # path('landing/', views.landing, name='landing'),
+
+]
